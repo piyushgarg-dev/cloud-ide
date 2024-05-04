6d82f41568abe8d6d8b120fba893cbf3de06864a
Beast <shadman.afzal.7@gmail.com>
2024-05-05 00:46:34 +0530

    add: more support file extension by react-ace

--- client/src/utils/getFileMode.js
+++ client/src/utils/getFileMode.js
@@ -1,22 +1,40 @@
 export const getFileMode = ({ selectedFile }) => {
-  const extension = selectedFile.split('.').reverse()[0];
+  const extension = selectedFile.split(".").reverse()[0];
 
   switch (extension) {
-    case 'js':
-      return 'javascript';
-    case 'py':
-      return 'python';
-    case 'java':
-      return 'java';
-    case 'cpp':
-      return 'cpp';
-    case 'c':
-      return 'c';
-    case 'json':
-      return 'json';
-    case 'rs':
-      return 'rust';
+    case "js":
+      return "javascript";
+    case "py":
+      return "python";
+    case "java":
+      return "java";
+    case "xml":
+      return "xml";
+    case "rb":
+      return "ruby";
+    case "sass":
+      return "sass";
+    case "md":
+      return "markdown";
+    case "sql":
+      return "mysql";
+    case "json":
+      return "json";
+    case "html":
+      return "html";
+    case "hbs":
+      return "handlebars";
+    case "handlebars":
+      return "handlebars";
+    case "go":
+      return "golang";
+    case "cs":
+      return "csharp";
+    case "litcoffee":
+      return "coffee";
+    case "css":
+      return "css";
     default:
-      return 'text';
+      return "";
   }
 };
